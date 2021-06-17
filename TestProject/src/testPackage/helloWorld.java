af8b29e3f1085f6edb4dd51ec2ee0dc43324f261
Tyrone_kyle654 <tyrone.f2000@outlook.com>
2021-06-17 14:55:21 +0100

    run this to test, eclipse is configured

--- TestProject/src/testPackage/helloWorld.java
+++ TestProject/src/testPackage/helloWorld.java
@@ -6,4 +6,9 @@ public class helloWorld {
 		// TODO Auto-generated constructor stub
 	}
 
+	public static void main(String[] args) {
+		// Prints "Hello, World" to the terminal window.
+		System.out.println("Hello World!");
+	}
+
 }
